2956b11bf7022eb5c20275111953e0e2f3385715
leahgevans <levans50@vols.utk.edu>
2023-07-06 19:30:56 -0400

    new image rec file

--- PI/ImgRec.py
+++ PI/ImgRec.py
@@ -3,25 +3,31 @@ import argparse
 import supervision as sv
 
 from ultralytics import YOLO
-CAM_1 = 1
+CAM_1 = 0
 
 cam = cv2.VideoCapture(CAM_1)
 
-model = YOLO("C:/Users/edwar/OneDrive/Desktop/repositories/IEEE2023/PI/beast.pt")
+model = YOLO("best (1).pt")
 
 box_ann = sv.BoxAnnotator(thickness = 2, text_thickness = 2, text_scale = 1)
 
 while True:
     ret, frame = cam.read()
 
+
     result = model(frame)[0]
+    names = model.names
+    for r in result:
+        for c in r.boxes.cls:
+            print(names[int(c)])
+    
     detections = sv.Detections.from_yolov8(result)
-    labels = [
-        f"{model.model.names[class_id]}"
-        for _, class_id, _
-        in detections
-    ]
-    frame = box_ann.annotate(scene=frame, detections = detections, labels = labels)
+    # labels = [
+    #     f"{model.model.names[class_id]}"
+    #     for _, class_id, _
+    #     in detections
+    # ]
+    frame = box_ann.annotate(scene=frame, detections = detections) #, labels = labels)
 
     cv2.imshow('yolov8', frame)
     if(cv2.waitKey(30) == 27):
